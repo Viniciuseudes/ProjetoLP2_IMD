631f13c550cd0145ae27738ee0f5f4adacc5dc78
Viniciuseudes <vinicius.egf13@gmail.com>
2023-12-15 20:32:45 -0300

    feat: Implementa a Classe Soja

--- InsumosAgro/src/classes/Soja.java
+++ InsumosAgro/src/classes/Soja.java
@@ -1,4 +1,25 @@
 package classes;
 
-public class Soja {
+public class Soja extends ProdutoComPeso {
+
+    private String tipo;
+
+    public Soja (String tipo, String nome, String peso, float valor, int qEstoque) {
+        super(nome, peso, valor, qEstoque);
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public void detalhes(){
+        System.out.println("Tipo (Farelo ou Casca): " + getTipo());
+        super.detalhes();
+    }
 }
