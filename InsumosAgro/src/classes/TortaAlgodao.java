a5aa06e989b9a2882b30f7ea0746741d0d4eeab9
Viniciuseudes <vinicius.egf13@gmail.com>
2023-12-15 20:24:12 -0300

    feat: Implementa a Classe TortaAlgodao

--- InsumosAgro/src/classes/TortaAlgodao.java
+++ InsumosAgro/src/classes/TortaAlgodao.java
@@ -1,4 +1,25 @@
 package classes;
 
-public class TortaAlgodao {
+public class TortaAlgodao extends ProdutoComPeso {
+
+    private String tipo;
+
+    public TortaAlgodao (String tipo, String nome, String peso, float valor, int qEstoque) {
+        super(nome, peso, valor, qEstoque);
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public void detalhes(){
+        System.out.println("Tipo (Moida ou Não Moida): " + getTipo());
+        super.detalhes();
+    }
 }
