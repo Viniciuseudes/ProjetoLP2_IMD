eae22b841f4dc23892928ad8290d7253882d1125
Viniciuseudes <vinicius.egf13@gmail.com>
2023-12-15 20:23:14 -0300

    feat: Implementa a Classe Sorgo

--- InsumosAgro/src/classes/Sorgo.java
+++ InsumosAgro/src/classes/Sorgo.java
@@ -1,4 +1,25 @@
 package classes;
 
-public class Sorgo {
+public class Sorgo extends ProdutoComPeso {
+
+    private String tipo;
+
+    public Sorgo (String tipo, String nome, String peso, float valor, int qEstoque) {
+        super(nome, peso, valor, qEstoque);
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public void detalhes(){
+        System.out.println("Tipo (Semente ou Ração): " + getTipo());
+        super.detalhes();
+    }
 }
