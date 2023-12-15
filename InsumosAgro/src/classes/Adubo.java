e74368009b449a91af34fea0b0fa71fd8f66ebc1
Viniciuseudes <vinicius.egf13@gmail.com>
2023-12-15 20:37:38 -0300

    feat: Implementa a Classe Adubo

--- InsumosAgro/src/classes/Adubo.java
+++ InsumosAgro/src/classes/Adubo.java
@@ -1,4 +1,35 @@
 package classes;
 
-public class Adubo {
+public class Adubo extends ProdutoComPeso {
+
+    private String tipo;
+    private boolean organico;
+
+    public Adubo(String tipo, String nome, String peso, float valor, int qEstoque, boolean organico) {
+        super(nome, peso, valor, qEstoque);
+        this.tipo = tipo;
+        this.organico = organico;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public boolean isOrganico() {
+        return organico;
+    }
+
+    public void setOrganico(boolean organico) {
+        this.organico = organico;
+    }
+
+    public void detalhes() {
+        System.out.println("Tipo de Adubo: " + getTipo());
+        System.out.println("Orgânico: " + (isOrganico() ? "Sim" : "Não"));
+        super.detalhes();
+    }
 }
