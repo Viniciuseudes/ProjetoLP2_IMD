0c671e078a96338c0e825647ddeaa86fbef592bb
Viniciuseudes <vinicius.egf13@gmail.com>
2023-12-15 19:38:49 -0300

    feat: Implementa ProdutoComPeso

--- InsumosAgro/src/classes/ProdutoSemPeso.java
+++ InsumosAgro/src/classes/ProdutoSemPeso.java
@@ -1,4 +1,23 @@
 package classes;
 
-public class ProdutoSemPeso {
+public class ProdutoSemPeso extends Produto {
+    private float volumeEmLitros;
+
+    public ProdutoSemPeso(String nome, float valor, int qEstoque, float volumeEmLitros) {
+        super(nome, valor, qEstoque);
+        this.volumeEmLitros = volumeEmLitros;
+    }
+
+    public float getVolumeEmLitros() {
+        return volumeEmLitros;
+    }
+
+
+    public void detalhes() {
+        System.out.println("Detalhes do Produto");
+        System.out.println("Nome: " + getNome());
+        System.out.println("volume: " + getVolumeEmLitros());
+        System.out.println("Quantidade em Estoque: " + getqEstoque());
+        System.out.println("Valor: " + getValor());
+    }
 }
