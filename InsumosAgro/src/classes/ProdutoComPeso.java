fd32b473bae49ebd1fe0d75439d546011baa4030
Viniciuseudes <vinicius.egf13@gmail.com>
2023-12-15 19:39:10 -0300

    feat: Implementa ProdutoComPeso

--- InsumosAgro/src/classes/ProdutoComPeso.java
+++ InsumosAgro/src/classes/ProdutoComPeso.java
@@ -1,4 +1,26 @@
 package classes;
 
-public class ProdutoComPeso {
+import classes.Produto;
+
+public abstract class ProdutoComPeso extends Produto {
+    private String peso;
+
+    public ProdutoComPeso(String nome, String peso, float valor, int qEstoque) {
+        super(nome, valor, qEstoque);
+        this.peso = peso;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+
+    public void detalhes() {
+        System.out.println("Detalhes do ProdutoComPeso");
+        System.out.println("Nome: " + getNome());
+        System.out.println("Peso: " + getPeso());
+        System.out.println("Quantidade em Estoque: " + getqEstoque());
+        System.out.println("Valor: " + getValor());
+
+    }
 }
