a6134ecc673e4a6930369552c4f75986ed2f06d4
Viniciuseudes <vinicius.egf13@gmail.com>
2023-12-16 01:40:39 -0300

    feat : Adicionando caixas ao restante do menu

--- InsumosAgro/src/classes/Principal.java
+++ InsumosAgro/src/classes/Principal.java
@@ -64,12 +64,16 @@ public class Principal {
                         break;
 
                     case 2:
-                        System.out.println(" ");
-                        System.out.println("Excluindo Cliente");
+                        System.out.println("\033[1;32m");
+                        System.out.println("\n╔════════════════════════════╗");
+                        System.out.println("║    Excluindo Cliente       ║");
+                        System.out.println("╚════════════════════════════╝");
+                        System.out.println("\033[0m");
                         do {
-                            System.out.println(" ");
-                            System.out.println("1 - Excluir Cliente");
-                            System.out.println("0 - Retornar ao menu");
+                            System.out.println("╔════════════════════════════╗");
+                            System.out.println("║1 - Excluir Cliente         ║");
+                            System.out.println("║0 - Retornar ao menu        ║");
+                            System.out.println("╚════════════════════════════╝");
                             menu = manFun.leitura.nextInt();
                             switch (menu) {
                                 case 1:
@@ -88,13 +92,17 @@ public class Principal {
                         break;
 
                     case 3:
-                        System.out.println(" ");
-                        System.out.println("Listar Cliente");
+                        System.out.println("\033[1;32m");
+                        System.out.println("\n╔════════════════════════════╗");
+                        System.out.println("║    Listando Cliente        ║");
+                        System.out.println("╚════════════════════════════╝");
+                        System.out.println("\033[0m");
                         do {
-                            System.out.println(" ");
-                            System.out.println("1 - Listar Clientes");
-                            System.out.println("2 - Listar cliente por nome");
-                            System.out.println("0 - Retornar ao menu");
+                            System.out.println("╔════════════════════════════╗");
+                            System.out.println("║1 - Listar Clientes         ║");
+                            System.out.println("║2 - Listar cliente por nome ║");
+                            System.out.println("║0 - Retornar ao menu        ║");
+                            System.out.println("╚════════════════════════════╝");
                             menu = manFun.leitura.nextInt();
                             switch (menu) {
                                 case 1:
@@ -117,17 +125,21 @@ public class Principal {
                         break;
 
                     case 4:
-                        System.out.println(" ");
-                        System.out.println("Cadastrando Produto");
+                        System.out.println("\033[1;32m");
+                        System.out.println("\n╔════════════════════════════╗");
+                        System.out.println("║    Cadastrando Produto     ║");
+                        System.out.println("╚════════════════════════════╝");
+                        System.out.println("\033[0m");
                         do {
-                            System.out.println(" ");
-                            System.out.println("1 - MILHO");
-                            System.out.println("2 - SORGO");
-                            System.out.println("3 - SOJA");
-                            System.out.println("4 - TORTA DE ALGODÃO");
-                            System.out.println("5 - ADUBO");
-                            System.out.println("6 - GLISOFATO");
-                            System.out.println("0 - Retornar ao menu");
+                            System.out.println("╔════════════════════════════╗");
+                            System.out.println("║1 - MILHO                   ║");
+                            System.out.println("║2 - SORGO                   ║");
+                            System.out.println("║3 - SOJA                    ║");
+                            System.out.println("║4 - TORTA DE ALGODÃO        ║");
+                            System.out.println("║5 - ADUBO                   ║");
+                            System.out.println("║6 - GLISOFATO               ║");
+                            System.out.println("║0 - Retornar ao menu        ║");
+                            System.out.println("╚════════════════════════════╝");
                             menu = manFun.leitura.nextInt();
                             switch (menu) {
                                 case 1:
@@ -159,13 +171,17 @@ public class Principal {
                         break;
 
                     case 5:
-                        System.out.println(" ");
-                        System.out.println("Listando Produtos");
+                        System.out.println("\033[1;32m");
+                        System.out.println("\n╔════════════════════════════╗");
+                        System.out.println("║    Listando Produto        ║");
+                        System.out.println("╚════════════════════════════╝");
+                        System.out.println("\033[0m");
                         do {
-                            System.out.println(" ");
-                            System.out.println("1 - Produto com Peso");
-                            System.out.println("2 - Produto sem Peso");
-                            System.out.println("0 - Retornar ao menu");
+                            System.out.println("╔════════════════════════════╗");
+                            System.out.println("║1 - Produto com Peso        ║");
+                            System.out.println("║2 - Produto sem Peso        ║");
+                            System.out.println("║0 - Retornar ao menu        ║");
+                            System.out.println("╚════════════════════════════╝");
                             menu = manFun.leitura.nextInt();
                             switch (menu) {
                                 case 1:
@@ -194,10 +210,15 @@ public class Principal {
                             System.out.println("Nenhuma produto cadastrado!!");
                         } else {
                             do {
-                                System.out.println(" ");
-                                System.out.println("Realizando Venda");
-                                System.out.println("1 - Realizar venda");
-                                System.out.println("0 - Retornar ao menu");
+                                System.out.println("\033[1;32m");
+                                System.out.println("\n╔════════════════════════════╗");
+                                System.out.println("║   Realizando Venda         ║");
+                                System.out.println("╚════════════════════════════╝");
+                                System.out.println("\033[0m");
+                                System.out.println("╔════════════════════════════╗");
+                                System.out.println("║1 - Realizar venda          ║");
+                                System.out.println("║0 - Retornar ao menu        ║");
+                                System.out.println("╚════════════════════════════╝");
                                 menu = manFun.leitura.nextInt();
                                 switch (menu) {
                                     case 1:
@@ -221,8 +242,11 @@ public class Principal {
                         if (manFun.vendas.isEmpty()) {
                             System.out.println("Nenhuma produto vendido!!");
                         } else {
-                            System.out.println(" ");
-                            System.out.println("Relatorio de Vendas");
+                            System.out.println("\033[1;32m");
+                            System.out.println("\n╔════════════════════════════╗");
+                            System.out.println("║    Relatório de vendas     ║");
+                            System.out.println("╚════════════════════════════╝");
+                            System.out.println("\033[0m");
                             manFun.relatorioVendas();
                             System.out.println("0 - Retornar ao menu");
                             menu = manFun.leitura.nextInt();
