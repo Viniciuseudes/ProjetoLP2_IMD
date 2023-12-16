d1ae260f92274007de7a21e768519b65eb9dd89a
Viniciuseudes <vinicius.egf13@gmail.com>
2023-12-15 21:00:58 -0300

    feat: implementa cadastro de Produto no menu

--- InsumosAgro/src/classes/Principal.java
+++ InsumosAgro/src/classes/Principal.java
@@ -7,24 +7,35 @@ public class Principal {
 
         int opcao, menu;
 
-        do{
-            System.out.println(" ");
-            System.out.println("Digite a opção desejada");
-            System.out.println("1 - Cadastrar Cliente");
-            System.out.println("2 - Excluir Cliente");
-            System.out.println("3 - Listar Cliente");
-            System.out.println("0 - Finalizar Sistema");
+
+        do {
+
+            System.out.println("\033[1;33m");
+            System.out.println("\n╔════════════════════════════╗");
+            System.out.println("║         MENU PRINCIPAL     ║");
+            System.out.println("╚════════════════════════════╝");
+            System.out.println("\033[0m");
+            System.out.println("╔════════════════════════════╗");
+            System.out.println("║ 1 - Cadastrar Cliente      ║");
+            System.out.println("║ 2 - Excluir Cliente        ║");
+            System.out.println("║ 3 - Listar Cliente         ║");
+            System.out.println("║ 4 - Cadastrar Produto      ║");
+            System.out.println("║ 0 - Finalizar Sistema      ║");
+            System.out.println("╚════════════════════════════╝");
             opcao = manFun.leitura.nextInt();
-            System.out.println(" ");
 
             switch(opcao){
                 case 1:
-                    System.out.println(" ");
-                    System.out.println("Cadastrando Cliente");
+                    System.out.println("\033[1;32m");
+                    System.out.println("\n╔════════════════════════════╗");
+                    System.out.println("║    Cadastrando Cliente     ║");
+                    System.out.println("╚════════════════════════════╝");
+                    System.out.println("\033[0m");
                     do{
-                        System.out.println(" ");
-                        System.out.println("1 - Cadastrar Cliente");
-                        System.out.println("0 - Retornar ao menu");
+                        System.out.println("╔════════════════════════════╗");
+                        System.out.println("║ 1 - Cadastrar Cliente      ║");
+                        System.out.println("║ 0 - Retornar ao menu       ║");
+                        System.out.println("╚════════════════════════════╝");
                         menu = manFun.leitura.nextInt();
                         switch(menu){
                             case 1:
@@ -92,6 +103,24 @@ public class Principal {
                             default:
                                 System.out.println(" ");
                                 System.out.println("Opção Invalida!!");
+                        }
+                    }while(menu != 0);
+                    break;
+
+                case 4:
+                    System.out.println(" ");
+                    System.out.println("Cadastrando Produto");
+                    do{
+                        System.out.println(" ");
+                        System.out.println("1 - MILHO");
+                        System.out.println("2 - ");
+                        System.out.println("0 - Retornar ao menu");
+                        menu = manFun.leitura.nextInt();
+                        switch(menu){
+                            case 1:
+                                System.out.println(" ");
+                                manFun.cadastrarMilho();
+                                break;
                         }
                     }while(menu != 0);
                     break;
